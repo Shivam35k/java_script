15c6bc63757b58735ee68de168b6be62a4b541f9
Shivam35k <114252890+Shivam35k@users.noreply.github.com>
2023-12-02 14:21:59 +0000

    datatypes

---
+++ .devcontainer/datatypes.js
@@ -0,0 +1,43 @@
+//  primitive data type
+
+//  7 types : String,  Number,  Boolean, null, undefained, symbole, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+// const bigNumber = 23534534
+
+
+//  Reference type (non primitve)
+
+//  Array, Objects, Functions
+
+
+const heros = ["shivam", "anubhav", "shubham"]
+ let myObj={
+    name: "shiv",
+    age: 22,
+}
+
+
+const myFunction=function(){
+    console.log("Hello world");
+}
+
+console.log(typeof bigNumber);
+
+
+//  if you more study of datatype so plz go the website of (ECMA script language)
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
