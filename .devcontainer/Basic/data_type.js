3535810cc7c5826bc5fe65f9ecdec750ec7b4c4f
Shivam35k <114252890+Shivam35k@users.noreply.github.com>
2023-12-02 12:59:24 +0000

    coversion_operation

---
+++ .devcontainer/Basic/data_type.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code newer version 
+
+
+
+// alert( 3+3)// we are using node js, not brousr
+
+console.log(3+3)
+
+console.log("shiv")
+
+let name = "shivam"
+let age = 20
+let isLoggedIn = false
+
+
+// number => 2 to pqwer 53
+//  bigint
+// string =>""
+//  boolean => true or false
+// null => standalon value
+//  undefine =>  mens abhi value define nhi h
+// symbol=> uniknes 
+
+
+// object
+
+
+
+console.log(typeof undefaine);
+console.log(typeof null);
--- .devcontainer/Basic/variable.js
+++ .devcontainer/Basic/variable.js
@@ -2,6 +2,7 @@ const accountId = 144553
 let accountEmail = "shiv@gmail.com"
 var accountPassward = "1234"
 accountCity = "prayagraj"
+let accountState;
 
 // accountId=2 // not allowed
 // console.log(accountId);
@@ -11,4 +12,4 @@ accountPassward = "73274"
 accountCity = "delhi"
 
 console.log(accountId);
-console.table([accountId, accountEmail, accountPassward, accountCity])
+console.table([accountId, accountEmail, accountPassward, accountCity, accountState])
