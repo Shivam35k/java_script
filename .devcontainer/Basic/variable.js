fc2007286e9b87ba1bddba5113092cc77ed68f44
Shivam35k <114252890+Shivam35k@users.noreply.github.com>
2023-12-02 09:59:34 +0000

    data_type

---
+++ .devcontainer/Basic/variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "shiv@gmail.com"
+var accountPassward = "1234"
+accountCity = "prayagraj"
+
+// accountId=2 // not allowed
+// console.log(accountId);
+
+accountEmail = "sh@sh.com"
+accountPassward = "73274"
+accountCity = "delhi"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassward, accountCity])
