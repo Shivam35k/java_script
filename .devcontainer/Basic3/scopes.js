213e2c7a98025c7946a0839baa043664d608b122
Shivam35k <114252890+Shivam35k@users.noreply.github.com>
2023-12-04 14:16:58 +0000

    scopes

--- .devcontainer/Basic3/scopes.js
+++ .devcontainer/Basic3/scopes.js
@@ -13,12 +13,57 @@ if (true) {
      let a = 10
      const b = 20
     var c = 3
-    console.log("INNER: ", 10);
+    // console.log("INNER: ", 10);
     
 }
 
 
 
- console.log(a);
+//  console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+function one (){
+    const username = "shivam"
+
+    function two (){
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+
+    two()
+}
+
+// one()
+
+
+if (true){
+    const username = "shivam"
+    if(username=== "shivam"){
+        const website = " youtube"
+        // console.log(username + website);
+
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+// ++++++++++++++++++++++++ intersting +++++++++++++
+
+console.log(addone(5))
+function addone (num){
+    return num+1
+}
+
+//  addone(5)
+
+
+
+const addTwo = function(num){
+    return num +2
+}
+addTwo(5)
