1562f5ac7271f5b1fff8458340ac1f24eb7df326
Shivam35k <114252890+Shivam35k@users.noreply.github.com>
2023-12-04 09:40:35 +0000

    function

--- .devcontainer/Basic3/function.js
+++ .devcontainer/Basic3/function.js
@@ -33,6 +33,40 @@ function loginUserMessage(username){
     return`${username} just logged in`
 }
 
-console.log(loginUserMessage("shivam"))//jb ham yaha koi value nhi pass krte h to undefine pass kr deta h
+// console.log(loginUserMessage("shivam"))//jb ham yaha koi value nhi pass krte h to undefine pass kr deta h
 
 
+
+
+function calculateCartPrice(val1, val2, ...num1){
+  return num1
+}
+// console.log(calculateCartPrice(200, 400, 500));
+
+
+const user = {
+    username: "shivam",
+    price: 1999
+
+
+}
+function handleObject(anyobject){
+console.log(`usernsme is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+ 
+handleObject({
+    username: "shiv",
+    price: 333
+})
+
+const myNewArray = [200, 300, 4000, 6000]
+
+function returnSecondValue(getArray){
+   return getArray[2] 
+}
+
+// console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([200, 300,400,500]));
