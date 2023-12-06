78318e8706884da898776dffe9e205cc59523f60
Shivam35k <114252890+Shivam35k@users.noreply.github.com>
2023-12-06 05:13:51 +0000

    four

---
+++ .devcontainer/itraters/four.js
@@ -0,0 +1,21 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: 'seift by apple'
+}
+
+
+for (const key in myObject) {
+//    console.log(`${key} shortcut is for ${myObject[key]}`); 
+}
+
+
+const programing = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programing) {
+    
+            console.log(programing[key]);
+    }
+
+
