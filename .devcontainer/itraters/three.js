81cb0c75b9f7eccfbb2fa2b4d22458bb0d8422ca
Shivam35k <114252890+Shivam35k@users.noreply.github.com>
2023-12-06 04:48:23 +0000

    three

---
+++ .devcontainer/itraters/three.js
@@ -0,0 +1,46 @@
+//  for off
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    // console.log(num);
+}
+
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+
+    // console.log(`Each cahe is ${greet}`);
+    
+}
+
+
+
+// maps   ye unike value k liye jane jate h
+
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United state america")
+map.set('Fr', "France")
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-',  value);
+    
+}
+
+
+const myObject = {
+    'game1': 'nfs',
+    'game2': 'spiderman'
+}
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }
+
